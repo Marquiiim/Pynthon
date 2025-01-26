bbe4a8c1d334f868f6c0e4a93e5a3a48d75a2f42
Marcos Gabriel <marcosindev@gmail.com>
2025-01-26 16:07:38 -0300

    Testes

---
+++ Aprendizado/estruturarepeticao.py
@@ -0,0 +1,11 @@
+contador = 0
+
+while contador < 5:
+    print(contador)
+    contador += 1
+
+
+frutas = ['maça', 'banana', 'laranja']
+
+for fruta in frutas:
+    print(fruta)
---
+++ Aprendizado/strings.py
@@ -0,0 +1,7 @@
+saudacao = 'Olá, mundo!'
+
+print(len(saudacao))
+
+print(saudacao[0])
+
+print(saudacao[0:4])
